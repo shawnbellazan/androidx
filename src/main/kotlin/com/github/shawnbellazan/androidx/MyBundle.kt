e74ca17374c1466478627837be43139002c6a084
GitHub Action <action@github.com>
2023-03-03 16:15:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/shawnbellazan/androidx/MyBundle.kt
+++ src/main/kotlin/com/github/shawnbellazan/androidx/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shawnbellazan.androidx
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/shawnbellazan/androidx/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/shawnbellazan/androidx/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.shawnbellazan.androidx.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shawnbellazan.androidx.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
